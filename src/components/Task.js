5b230705a3552e489c09fed1a0f3872cbbc12c1e
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-11-16 10:20:05 +0700

    add props to select where the clicks belong to

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,11 @@
+import { FaTimes } from "react-icons/fa";
+
 const Task = (props) => {
 	return (
 		<div className="task">
-			<h3>{props.tasks.text}</h3>
+			<h3>
+				{props.tasks.text} <FaTimes style={{ color: "red", cursor: "pointer" }} onClick={() => props.onDelete(props.tasks.id)} />
+			</h3>
 			<p>{props.tasks.day}</p>
 		</div>
 	);
