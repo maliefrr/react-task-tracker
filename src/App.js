b83a88fd98c2bd9d2e1d7c89dbd4e62c7fd55e63
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-11-17 08:40:03 +0700

    add update reminder function

--- src/App.js
+++ src/App.js
@@ -1,57 +1,84 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false);
-	const [tasks, setTask] = useState([
-		{
-			id: 1,
-			text: "Belajar React.js",
-			day: "15 November 2021",
-			reminder: true,
-		},
-		{
-			id: 2,
-			text: "Membuat Paper Perancangan dan Strategi Sistem Informasi",
-			day: "16 November 2021",
-			reminder: true,
-		},
-		{
-			id: 3,
-			text: "Mencari Inspirasi Konsep Birthday Fiony",
-			day: "17 November 2021",
-			reminder: true,
-		},
-	]);
+	const [tasks, setTask] = useState([]);
 
-	const deleteTask = (id) => {
+	useEffect(() => {
+		const getTasks = async () => {
+			const taskFromServer = await fetchTasks();
+			setTask(taskFromServer);
+		};
+
+		getTasks();
+	}, []);
+
+	const fetchTasks = async () => {
+		const res = await fetch("http://localhost:5000/tasks");
+		const data = await res.json();
+
+		return data;
+	};
+
+	// Fetch Task
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`);
+		const data = await res.json();
+
+		return data;
+	};
+
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: "DELETE",
+		});
 		setTask(tasks.filter((el) => el.id !== id));
 	};
 
-	const toggleReminder = (id) => {
-		setTask(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
+	// Toggle Reminder
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id);
+		const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(updTask),
+		});
+
+		const data = await res.json();
+
+		setTask(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)));
 	};
 
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 1000) + 1;
-		const newTask = { id, ...task };
-		setTask([...tasks, newTask]);
+	const addTask = async (task) => {
+		const res = await fetch("http://localhost:5000/tasks", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(task),
+		});
+
+		const data = await res.json();
+
+		setTask([...tasks, data]);
+
+		// const id = Math.floor(Math.random() * 1000) + 1;
+		// const newTask = { id, ...task };
+		// setTask([...tasks, newTask]);
 	};
 
 	return (
 		<div className="container">
 			<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
 			{showAddTask ? <AddTask onAdd={addTask} /> : ""}
-			{tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-			) : (
-				<div className="alert alert-danger alert-dismissible fade show" role="alert">
-					No Task to Show
-					<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-				</div>
-			)}
+			{tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : <div>No Task to Show</div>}
 		</div>
 	);
 }
